955ed11c27060f07d6fe88c082825a49b88b8594
Tommaso Armstrong <tomm@tomma.so>
2016-03-04 09:55:00 +1100

    Search returns JSON from Guardian API

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fetch = require('node-fetch');
+require('dotenv').load()
+
 var app = express();
 
 app.get("/search", function (req,res) {
-    console.log("Search for", req.query.q);
-    res.send(req.query.q);
+
+    var text = req.query.q;
+
+    console.log("Search for", text);
+
+    guardianSearch(text).then(function (json) {
+        res.send(json);
+    });
 });
 
 app.use(express.static("app"));
@@ -11,3 +20,10 @@ app.use(express.static("app"));
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+var guardianSearch = function (text) {
+    return fetch("http://content.guardianapis.com/search?q=" + text +"&api-key=" + process.env.GUARDIAN_KEY)
+        .then(function (res) {
+            return res.json();
+        })
+}
