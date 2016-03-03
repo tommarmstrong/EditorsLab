85d63518417d1d5b4ed436ce1e626e2949b82e58
Tommaso Armstrong <tomm@tomma.so>
2016-03-04 09:14:59 +1100

    Basic editor functionality working
    LocalStorage implemented and selection detected on change

--- app/index.js
+++ app/index.js
@@ -7,4 +7,29 @@ HTMLTextAreaElement.prototype.getSelection = HTMLInputElement.prototype.getSelec
     return "";
 };
 
-////alert(document.getElementsByTagName("textarea")[0].getSelection());
+var title = $(".title");
+var text = $("textarea");
+var reset = $(".reset");
+
+text.on("select", function () {
+    console.log(this.getSelection());
+});
+
+text.on("input", function () {
+    localStorage.setItem("text", $(this).val());
+});
+
+title.on("input", function () {
+    localStorage.setItem("title", $(this).val());
+
+});
+
+reset.on("click", function () {
+    title.val("");
+    text.text("");
+});
+
+if (text.val() == "" && title.val() == "") {
+    title.val(localStorage.getItem("title") ? localStorage.getItem("title") : "");
+    text.text(localStorage.getItem("text") ? localStorage.getItem("text") : "");
+}
